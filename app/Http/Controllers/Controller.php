4a9f744d1d7964a887bd77afae0394b422adf0ac
Alina <alinasackovskaa425@gmail.com>
2024-04-03 16:54:53 +0300

    scrum-39 update main controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,34 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    /**
+     * @param  Request  $request
+     * @return  array
+     */
+    protected function ParseQuery(Request $request): array
+    {
+        if ($request->query->has('filter')) {
+            $filterParams = $request->query('filter');
+        } else {
+            $filterParams = [];
+        }
+        if ($request->query->has('column') && $request->query('column') != null) {
+            $column = $request->query('column');
+        } else {
+            $column = 'created_at';
+        }
+        if ($request->query->has('ascending')) {
+            $ascending = $request->query('ascending');
+            $ascending = $ascending == 'true' ? 'asc' : 'desc';
+        } else {
+            $ascending = 'desc';
+        }
+        return [
+            'filterParams' => $filterParams,
+            'column' => $column,
+            'ascending' => $ascending
+        ];
+    }
+
 }
