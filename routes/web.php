f4fbb68f4dd6fc9e1e1acff2c00634808cfd5fa0
Alina <alinasackovskaa425@gmail.com>
2024-04-06 08:42:11 +0300

    scrum-38 add client routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::middleware('auth')->group((function () {
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard')->middleware('can:view dashboard');
+    Route::resource('clients', ClientController::class)->except('show');
+    }));
+
+
+require __DIR__ . '/auth.php';
